e88dbad1c9d8101278e30ca41653d1994f7462c5
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-11-11 20:26:44 +0300

    all tasks

--- src/main/java/HttpUtil.java
+++ src/main/java/HttpUtil.java
@@ -1,7 +1,8 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.jsoup.Jsoup;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
@@ -9,6 +10,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HttpUtil {
     private static final HttpClient CLIENT = HttpClient.newHttpClient();
@@ -64,23 +66,74 @@ public class HttpUtil {
         System.out.println("response.body() = " + response.body());
     }
 
-    public void Task2(String uri) throws InterruptedException, IOException {
-        String response = Jsoup.connect(uri)
-                .ignoreContentType(true)
-                .get()
-                .body()
-                .text();
+    public void Task2(URI uri) throws InterruptedException, IOException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Content-type", "application/json")
+                .GET()
+                .build();
 
+        String response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
         Type type = TypeToken
-                .getParameterized(List.class, Comment.class)
+                .getParameterized(List.class, Post.class)
                 .getType();
 
-        List<Comment> comment = new Gson().fromJson(response, type);
 
+        List<Post> json = GSON.fromJson(response, type);
 
+        int x = 0;
+        int y = 0;
 
-        System.out.println("comment = " + comment);
+        for (Post p: json) {
+            if (p.getId() > y){
+                x = p.getUserId();
+                y = p.getId();
+            }
+        }
 
+        String commentUri = "https://jsonplaceholder.typicode.com/posts/" + y + "/comments";
+
+        HttpRequest requestComment = HttpRequest.newBuilder()
+                .uri(URI.create(commentUri))
+                .header("Content-type", "application/json")
+                .GET()
+                .build();
+
+        String responseComment = CLIENT.send(requestComment, HttpResponse.BodyHandlers.ofString()).body();
+
+        try (FileWriter writer = new FileWriter(new File("user-" + x + "-post-" + y + "-comments.json")))
+        {
+            writer.write(responseComment);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    public void Task3(int namberOfUser) throws IOException, InterruptedException {
+        String uri = "https://jsonplaceholder.typicode.com/users/" + namberOfUser + "/todos";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-type", "application/json")
+                .GET()
+                .build();
+
+        String response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+        Type type = TypeToken
+                .getParameterized(List.class, Todo.class)
+                .getType();
+
+
+        List<Todo> json = GSON.fromJson(response, type);
+        List<Todo> filterJson = json.stream().filter(item -> !item.getCompleted()).collect(Collectors.toList());
+
+        for (Todo j: filterJson) {
+            System.out.println(j);
+        }
+    }
+
+
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,52 +9,55 @@ public class Test {
     public static String GET_ALL_URI = "https://jsonplaceholder.typicode.com/users";
     public static String GET_USERID_URI = "https://jsonplaceholder.typicode.com/users?id=1";
     public static String GET_USERNAME_URI = "https://jsonplaceholder.typicode.com/users?username=Bret";
-    public static String GET_COMMENTS_URI = "https://jsonplaceholder.typicode.com/posts/10/comments";
+    public static String GET_POSTS_URI = "https://jsonplaceholder.typicode.com/users/1/posts";
 
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-//        User user = new User();
-//        user.setId(10);
-//        user.setName("Kolya");
-//        user.setUsername("Fix");
-//        user.setEmail("KolyaFix@oil.org");
-//        user.setAddress(Map.of("street", "Kulas Light",
-//                "suite", "Apt. 556",
-//                "city", "Gwenborough",
-//                "zipcode", "92998-3874",
-//                "geo", String.valueOf(Map.of(
-//                    "lat", "-37.3159",
-//                    "lng", "81.1496")))
-//                );
-//        user.setPhone("010-692-6593 x09125");
-//        user.setWebsite("hildegard.org");
-//        user.setCompany(Map.of("name", "Romaguera-Crona",
-//                "catchPhrase", "Multi-layered client-server neural-net",
-//                "bs", "harness real-time e-markets"));
-//
-//        //1 Task
-//        HttpUtil httpUtil = new HttpUtil();
-//        httpUtil.methodPost(URI.create(POST_URI), user);
-//
-//        //2Task
-//        user.setName("Yaroslav");
-//        httpUtil.methodPut(URI.create(PUT_URI), user);
-//
-//        //3Task
-//        httpUtil.methodDelete(URI.create(PUT_URI));
-//
-//        //4Task
-//        httpUtil.methodInformAllUsers(URI.create(GET_ALL_URI));
-//
-//        //5Task
-//        httpUtil.methodInformAllUsers(URI.create(GET_USERID_URI));
-//
-//        //6Task
-//        httpUtil.methodInformAllUsers(URI.create(GET_USERNAME_URI));
+        User user = new User();
+        user.setId(10);
+        user.setName("Kolya");
+        user.setUsername("Fix");
+        user.setEmail("KolyaFix@oil.org");
+        user.setAddress(Map.of("street", "Kulas Light",
+                "suite", "Apt. 556",
+                "city", "Gwenborough",
+                "zipcode", "92998-3874",
+                "geo", String.valueOf(Map.of(
+                    "lat", "-37.3159",
+                    "lng", "81.1496")))
+                );
+        user.setPhone("010-692-6593 x09125");
+        user.setWebsite("hildegard.org");
+        user.setCompany(Map.of("name", "Romaguera-Crona",
+                "catchPhrase", "Multi-layered client-server neural-net",
+                "bs", "harness real-time e-markets"));
 
+        //Task 1.1
         HttpUtil httpUtil = new HttpUtil();
-        httpUtil.Task2(GET_COMMENTS_URI);
+        httpUtil.methodPost(URI.create(POST_URI), user);
+
+        //Task 1.2
+        user.setName("Yaroslav");
+        httpUtil.methodPut(URI.create(PUT_URI), user);
+
+        //Task 1.3
+        httpUtil.methodDelete(URI.create(PUT_URI));
+
+        //Task 4.1
+        httpUtil.methodInformAllUsers(URI.create(GET_ALL_URI));
+
+        //Task 5.1
+        httpUtil.methodInformAllUsers(URI.create(GET_USERID_URI));
+
+        //Task 1.6
+        httpUtil.methodInformAllUsers(URI.create(GET_USERNAME_URI));
+
+        //Task 2
+        httpUtil.Task2(URI.create(GET_POSTS_URI));
+
+        //Task 3
+        httpUtil.Task3(1);
     }
 }
